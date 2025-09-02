a9a21a78a2ea23f56426ae5a61c9f58be7a637d4
Sorawit028 <tonkwaochaiwan@gmail.com>
2025-09-02 09:43:37 +0700

    Loop028

---
+++ pts02807011_Loop.py
@@ -0,0 +1,9 @@
+sint='marcuscode'
+for n in sint:
+    print(n)
+    names=['Mateo','John','Eric','Robert']
+    for n in names:
+        print(n)
+    numbers=[10,20,30,40,50,60,70,80]
+    for n in numbers:
+        print(n)
